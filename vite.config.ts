e2af31ff16c0689798ce2deea9d0d760f833f017
Kilyion Romary <93580066+Kilyion04@users.noreply.github.com>
2025-05-13 14:16:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Lovable",
+  base: "/Lovable/",
   server: {
     host: "::",
     port: 8080,
